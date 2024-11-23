b92f0bfaf9a18f2513790bd1ec28bbc4e6f89de8
ethonimus <48042245+ethoart@users.noreply.github.com>
2024-11-23 22:45:47 +0530

    Update contracts.ts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -5,7 +5,7 @@ import { stakingABI } from "./stakingABI";
 
 const nftContractAddress = "0x282082C62Cd03eDA3A61AF62584cAb1C57e8d338";
 const rewardTokenContractAddress = "0x8178A20c5EE42EDBfB5DeF23b9B5DaD6986f5003";
-const stakingContractAddress = "0x22B3b2AD8962657C865157eA2793717F9321EB0D";
+const stakingContractAddress = "0xcB3b5478aE517F75Cd473F7dc822daAE72CD1071";
 
 export const NFT_CONTRACT = getContract({
     client: client,
