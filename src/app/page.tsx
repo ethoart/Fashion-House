2790c0e9a70cb0e3d34519cee05af5934a565233
ethonimus <48042245+ethoart@users.noreply.github.com>
2024-11-23 21:52:12 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { ConnectEmbed } from "@/app/thirdweb";
 import { client } from "./client";
 import { chain } from "./chain";
 import { Staking } from "../../components/Staking";
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -12,7 +13,10 @@ export default function Home() {
       margin: "20px auto",
       width: "500px",
     }}>
-      <h1>ERC-721 Staking App</h1>
+      <Head>
+  <link rel="icon" href="https://raw.githubusercontent.com/ethoart/manikeimages/refs/heads/main/logo%20w.png" />
+</Head>
+      <h1>Fashion House</h1>
       <ConnectEmbed
         client={client}
         chain={chain}
