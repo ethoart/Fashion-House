32162ab553050403669896bbbb39e0cc55720d3a
ethonimus <48042245+ethoart@users.noreply.github.com>
2024-11-23 21:43:34 +0530

    Update contracts.ts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -3,9 +3,9 @@ import { client } from "@/app/client";
 import { getContract } from "thirdweb";
 import { stakingABI } from "./stakingABI";
 
-const nftContractAddress = "<contract_address>";
-const rewardTokenContractAddress = "<contract_address>";
-const stakingContractAddress = "<contract_address>";
+const nftContractAddress = "0x282082C62Cd03eDA3A61AF62584cAb1C57e8d338";
+const rewardTokenContractAddress = "0x8178A20c5EE42EDBfB5DeF23b9B5DaD6986f5003";
+const stakingContractAddress = "0x22B3b2AD8962657C865157eA2793717F9321EB0D";
 
 export const NFT_CONTRACT = getContract({
     client: client,
