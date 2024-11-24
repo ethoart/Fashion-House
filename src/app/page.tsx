4b2770f8a3a3b5ded4c280085e2e972290285903
ethonimus <48042245+ethoart@users.noreply.github.com>
2024-11-24 06:02:09 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { ConnectEmbed } from "@/app/thirdweb";
 import { client } from "./client";
 import { chain } from "./chain";
 import { Staking } from "../../components/Staking";
+import React from 'react';
 import Head from 'next/head';
 
 export default function Home() {
